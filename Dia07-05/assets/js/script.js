97422725607ae82250e962095191e1e8ead65963
Miguel Vargas Ferreira <miguelferreira8967@gmail.com>
2026-05-07 17:07:42 -0300

    feat: Adiciona atividades do dia 07/05

---
+++ Dia07-05/assets/js/script.js
@@ -0,0 +1,37 @@
+const div = document.querySelector('div');
+const button = document.querySelector('button');
+// let tarefa = 1;
+let contador = 0;
+let numeroDeItens = 0;
+// button.addEventListener('click', () =>{
+
+// const novo = document.createElement('li')
+// novo.textContent = `${tarefa++} tarefa`
+// div.appendChild(novo);
+
+// novo.addEventListener('click', () => {
+//         novo.remove();
+//         tarefa--;
+      
+    
+// })
+
+
+// })
+
+
+// button.addEventListener("click", () => {
+//     if(numeroDeItens == 0){
+//         numero = numeroDeItens;
+
+//     }
+//     contador++
+//     const box = document.createElement("div")
+//     box.textContent = `${contador} `
+//     box.classList.add("container")    
+//     box.addEventListener("click", () => {
+//         box.remove()
+//         numeroDeItens--
+//     })
+//     div.appendChild(box)
+// })
